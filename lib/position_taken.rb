e709f975947ef7af9a6b9df9df3879ce1176b9ff
Makonsi <makonsi@gmail.com>
2016-03-18 13:10:47 +0000

    nitrous-backup at Fri Mar 18 13:10:47 UTC 2016

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+board=["X","","","","","","","",""]
+def position_taken?(board,position)
+if board [position.to_i] == ""
+        result= "good"
+        puts result
+else
+        result= "not good"
+        puts result
+end
+end
+position = gets. strip
+puts "u chose #{position}"
+position_taken?(board,position)
